276fa43019dac67a5aa3e732b72262c1748a0caf
Shimin Shuai <shuaishimin@gmail.com>
2016-10-13 18:13:30 -0400

    start the driver power
    create the repo

---
+++ src/DriverPower.py
@@ -0,0 +1,286 @@
+import helperDP as dp 
+import os
+import sys
+import warnings
+from datetime import datetime
+
+import pandas as pd
+import numpy as np
+import scipy as sp
+
+import sklearn
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.linear_model import LassoCV, RandomizedLasso
+
+import statsmodels
+from statsmodels.sandbox.stats.multicomp import multipletests
+import statsmodels.api as sm
+
+''' DriverPower - Find the cancer driver potential for genomic elements
+
+Steps:
+1. Read cg, ct, cv for training and test data
+2. Scaling
+3. Filter
+4. Feature selection
+5. Modelling
+'''
+
+def get_param():
+    path_cg_test  = '/Users/shimin/Desktop/R/MutationRate/COAD-US/exon/cg.exon.collapsed.tsv'
+    path_ct_test  = '/Users/shimin/Desktop/R/MutationRate/COAD-US/exon/COAD-READ.ct.exon.tsv'
+    path_cv_test  = '/Users/shimin/Desktop/R/MutationRate/covar_csv/cv1226_exon.csv'
+    path_cg_train = '/Users/shimin/Desktop/R/MutationRate/COAD-US/lincRNA_prom/cg.strat.tsv'
+    path_ct_train = '/Users/shimin/Desktop/R/MutationRate/COAD-US/lincRNA_prom/CRC.ct.strat.tsv'
+    path_cv_train = '/Users/shimin/Desktop/R/MutationRate/covar_csv/cv1226_strat.csv'
+    scaler_type   = 'robust'
+    skip_fs       = False # bool, skip feature selection
+    output_prefix = 'robust_strat'
+    output_dir    = './'
+    save_data     = True # bool, save pre-processed data
+    preprocessed  = False # whether or not the data is pre-processed (in .npy format)
+    scale_first   = True # True: scale before filter; False: filter before scale
+    assert scaler_type in ['standard', 'robust'], 'Scaler must be chosen from "standard" or "robust"'
+
+    # print params
+    print('Input parameters: ')
+    print('''
+        path_cg_test  = {}
+        path_ct_test  = {}
+        path_cv_test  = {}
+        path_cg_train = {}
+        path_ct_train = {}
+        path_cv_train = {}
+        scaler_type   = {}
+        skip_fs       = {}
+        output_prefix = {}
+        output_dir    = {}
+        save_data     = {}
+        preprocessed  = {}
+        scale_first   = {}
+        '''.format(path_cg_test, path_ct_test, path_cv_test, path_cg_train, path_ct_train,
+            path_cv_train, scaler_type, skip_fs, output_prefix, output_dir, save_data, preprocessed, scale_first))
+    print('Versions:')
+    print('- Python: ', sys.version)
+    print('- Statsmodels: ', statsmodels.__version__)
+    print('- Sklearn: ', sklearn.__version__)
+    print('- Numpy: ', np.__version__)
+    print('- Scipy: ', sp.__version__)
+    print('- Pandas: ', pd.__version__)
+    return (path_cg_test, path_ct_test, path_cv_test, path_cg_train, path_ct_train,
+        path_cv_train, scaler_type, skip_fs, output_prefix, output_dir, save_data, preprocessed, scale_first)
+
+def scaling(Xtrain, Xtest, scaler_type):
+    ''' Scale data with sklearn robust or standard scaler.
+    Args:
+        Xtrain, Xtest - numpy ndarray, each column represents one feature.
+        scaler_type - str, either 'robust' or 'standard'
+    Return:
+        scaled np.ndarray of Xtrain and Xtest
+    '''
+    assert scaler_type in ['standard', 'robust'], 'Scaler must be chosen from "standard" or "robust"'
+    if scaler_type == 'robust':
+        scaler = RobustScaler()
+    elif scaler_type == 'standard':
+        scaler = StandardScaler()
+    # same scaler fitted with Xtrain
+    scaler.fit(Xtrain)
+    return scaler.transform(Xtrain), scaler.transform(Xtest)
+
+def feature_selection(Xtrain, ytrain, fs_method, max_iter=2000, cv=5, alpha=0.00459451108436):
+    '''
+    '''
+    if fs_method == 'lassocv':
+        print('Implementing LassoCV with {} iter. and {}-fold CV'.format(max_iter, cv))
+        clf = LassoCV(max_iter=max_iter, cv=10)
+        lassocv = clf.fit(Xtrain, ytrain)
+        print('LassoCV alpha = ', lassocv.alpha_)
+        return lassocv, abs(lassocv.coef_)
+    elif fs_method == 'rndlasso':
+        # apply rndlasso
+        print('Implementing Random Lasso with alpha={}'.format(alpha))
+        clf = RandomizedLasso(alpha=alpha, sample_fraction=0.4, selection_threshold=1e-3, normalize=False)
+        rndlasso = clf.fit(Xtrain, ytrain)
+        return rndlasso, rndlasso.scores_
+
+def feature_score(fscore, fnames, cutoff):
+    ''' Return selected feature names and index vector
+    '''
+    fidx = np.where(fscore > cutoff)[0]
+    fset = fnames[fidx]
+    return fset, fidx
+
+def read_raw_data(path_cg_test, path_ct_test, path_cv_test, 
+    path_cg_train, path_ct_train, path_cv_train):
+    ''' Read cg ct and cv data from TSV or CSV files.
+    '''
+    print('Test Data')
+    cg_test,  ct_test,  cv_test  = dp.load_all(path_cg=path_cg_test,  path_ct=path_ct_test,  path_cv=path_cv_test)
+    print('Training Data')
+    cg_train, ct_train, cv_train = dp.load_all(path_cg=path_cg_train, path_ct=path_ct_train, path_cv=path_cv_train)
+    assert np.array_equal(cv_test.columns, cv_train.columns), 'Feature names do not match in Training and Testing data'
+    fnames = cv_test.columns.values   
+    return (cg_test, ct_test, cv_test.as_matrix(),
+        cg_train, ct_train, cv_train.as_matrix(), fnames)
+
+def read_npy_data(path_X_train, path_X_test, path_y_train, 
+    path_y_test, path_gnames, path_fnames, path_grecur):
+    ''' Read preprocessed X and y_binom .npy files.
+    '''
+    X_train = np.load(path_X_train)
+    X_test  = np.load(path_X_test)
+    ybinom_train = np.load(path_y_train)
+    ybinom_test  = np.load(path_y_test)
+    grecur = np.loadtxt(path_grecur)
+    gnames = np.genfromtxt(path_gnames, dtype=np.str)
+    fnames = np.genfromtxt(path_fnames, dtype=np.str)
+    return X_train, X_test, ybinom_train, ybinom_test, gnames, fnames, grecur
+
+def save_processed_data(X_train, X_test, ybinom_train, ybinom_test,
+    gnames, fnames, grecur, output_dir, output_prefix):
+    # Save X
+    Xtrain_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'Xtrain.npy']))
+    np.save(Xtrain_outpath, X_train)
+    Xtest_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'Xtest.npy']))
+    np.save(Xtest_outpath, X_test)
+    # Save y
+    ytrain_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'ytrain.npy']))
+    np.save(ytrain_outpath, ybinom_train)
+    ytest_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'ytest.npy']))
+    np.save(ytest_outpath, ybinom_test)
+    # Save gnames, fnames, grecur
+    gnames_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'test_bin_names.txt']))
+    pd.Series(gnames).to_csv(gnames_outpath, index=False)
+    fnames_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'feature_names.txt']))
+    pd.Series(fnames).to_csv(fnames_outpath, index=False)
+    grecur_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'test_bin_recur.txt']))
+    pd.Series(grecur).to_csv(grecur_outpath, index=False)
+    print('\n'.join([Xtrain_outpath, Xtest_outpath, ytrain_outpath, ytest_outpath, gnames_outpath, fnames_outpath, grecur_outpath]))
+    return 0
+
+def filter(ct_train, ct_test, cg_train, cg_test,
+    Xtrain, Xtest, ybinom_train, ybinom_test):
+    # obtain filter
+    print('Test data')
+    keep_test, grecur  = dp.get_filter(ct=ct_test, cg=cg_test, return_recur=True)
+    print('Train data')
+    keep_train = dp.get_filter(ct=ct_train, cg=cg_train)    
+    # apply filter
+    X_test,  ybinom_test  = dp.apply_filter(keep_test,  [Xtest,  ybinom_test])
+    X_train, ybinom_train = dp.apply_filter(keep_train, [Xtrain, ybinom_train])
+    gnames, grecur = dp.apply_filter(keep_test, [cg_test.index.values, np.array(grecur)])
+    return (X_test, ybinom_test, X_train, ybinom_train, gnames, grecur)
+
+def main():
+    print('DriverPower V0.1')
+    start = datetime.now()
+    print('Start at {}'.format(start))
+    ##
+    ## Get parameters
+    ##
+    print('='*5, 'Obtaining Parameters', '='*5)
+    (path_cg_test, path_ct_test, path_cv_test, path_cg_train, 
+        path_ct_train, path_cv_train, scaler_type, skip_fs, 
+        output_prefix, output_dir, save_data, preprocessed, scale_first) = get_param()
+    ##
+    ## Read data
+    ##
+    print('='*5, 'Reading data', '='*5)
+    if preprocessed:
+        (X_train, X_test, ybinom_train, ybinom_test,
+            gnames, fnames, grecur) = read_npy_data(path_X_train, path_X_test, path_y_train,
+            path_y_test, path_gnames, path_fnames, path_grecur)
+    else:
+        (cg_test, ct_test, X_test, cg_train, 
+            ct_train, X_train, fnames) = read_raw_data(path_cg_test, path_ct_test, 
+            path_cv_test, path_cg_train, path_ct_train, path_cv_train)
+
+    if not preprocessed:
+        ##
+        ## Get response
+        ##
+        print('='*5, 'Getting response', '='*5)
+        print('Test data')
+        ybinom_test = dp.get_response(ct_test, cg_test)
+        print('Training data')
+        ybinom_train = dp.get_response(ct_train, cg_train)
+        ##
+        ## Scale and Filter
+        ##
+        if scale_first:
+            # scale
+            print('='*5, 'Applying {} scaler'.format(scaler_type), '='*5)
+            X_train, X_test = scaling(X_train, X_test, scaler_type)
+            # filter
+            print('='*5, 'Applying filter', '='*5)
+            (X_test, ybinom_test, X_train, ybinom_train, gnames, grecur) = filter(ct_train, ct_test, cg_train, cg_test,
+                X_train, X_test, ybinom_train, ybinom_test)
+        else:
+            # filter
+            print('='*5, 'Applying filter', '='*5)
+            (X_test, ybinom_test, X_train, ybinom_train, gnames, grecur) = filter(ct_train, ct_test, cg_train, cg_test,
+                X_train, X_test, ybinom_train, ybinom_test)
+            # scale
+            print('='*5, 'Applying {} scaler'.format(scaler_type), '='*5)
+            X_train, X_test = scaling(X_train, X_test, scaler_type)                        
+        ##
+        ## Save data
+        ##
+        if save_data:
+            print('='*5, 'Saving prepocessed data', '='*5)
+            save_processed_data(X_train, X_test, ybinom_train, ybinom_test,
+                gnames, fnames, grecur, output_dir, output_prefix)
+    # More reponse
+    ymu_train = ybinom_train[:,0] * 1.0 / ybinom_train.sum(1)
+    ylogit_train = sp.special.logit(ymu_train)
+    # ymu_test = ybinom_test[:,0] * 1.0 / ybinom_test.sum(1)
+    # ylogit_test = sp.special.logit(ymu_test)
+    ##
+    ## Feature selection
+    ##
+    if skip_fs:
+        print('='*5, 'Skip feature selection', '='*5)
+    else:
+        print('='*5, 'Feature Selection', '='*5)
+        lassocv,  fscore_lassocv   = feature_selection(X_train, ylogit_train, 'lassocv')
+        rndlasso, fscore_rndlasso  = feature_selection(X_train, ylogit_train, 'rndlasso', alpha=lassocv.alpha_)       
+        # select feature by cutoff
+        cutoff_rndlasso = 0.5
+        fset_rndlasso, fidx_rndlasso = feature_score(fscore_rndlasso, fnames, cutoff=cutoff_rndlasso)
+        print('RNDLasso - at cutoff={}, {} selected features are: {}'.format(cutoff_rndlasso, fidx_rndlasso.shape[0], ', '.join(fset_rndlasso)))
+        cutoff_lassocv  = 0.001
+        fset_lassocv, fidx_lassocv = feature_score(fscore_lassocv, fnames, cutoff=cutoff_lassocv)
+        print('LassoCV - at cutoff={}, {} selected features are: {}'.format(cutoff_lassocv, fidx_lassocv.shape[0], ', '.join(fset_lassocv)))
+        # write fs results to file (tsv)
+        fs_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'feature_selection_res.tsv']))
+        print('Saving feature selection results to {}'.format(fs_outpath))
+        fs_res = pd.DataFrame({'fname': fnames, 'lassocv': fscore_lassocv, 'rndlasso': fscore_rndlasso})
+        fs_res.to_csv(fs_outpath, sep='\t', index=False)
+    ##
+    ## Model
+    ##
+    print('='*5, 'Predicting mutation rate', '='*5)
+    X_train = sm.add_constant(X_train[:, fidx_rndlasso], prepend=False)
+    X_test  = sm.add_constant(X_test[:,  fidx_rndlasso], prepend=False)
+    glm = sm.GLM(ybinom_train, X_train, family=sm.families.Binomial())
+    glm_res = glm.fit()
+    mu_pred = glm_res.predict(X_test)
+    pval = [sp.stats.binom_test(x, n, p, 'greater') for x, n, p in zip(ybinom_test[:, 0], ybinom_test.sum(1), mu_pred)]
+    qval = multipletests(pval, method='fdr_bh')[1]
+    glm_res = pd.DataFrame({'binID': gnames, 'Score': -np.log10(pval), 'Pval': pval, 'Qval': qval,
+                     'Length': ybinom_test.sum(1), 'nMut':ybinom_test[:, 0], 'Recur': grecur, 'Mu': mu_pred})
+    glm_res = glm_res.sort_values('Pval')
+    glm_res.set_index('binID', inplace=True)
+    # write result to file
+    mod_outpath = os.path.join(output_dir, '_'.join([output_prefix, 'glm_res.tsv']))
+    print('Saving GLM results to {}'.format(mod_outpath))
+    glm_res.to_csv(mod_outpath, sep='\t', index=True)
+    ##
+    ## END
+    ##
+    end = datetime.now()
+    print('End at {}'.format(end))
+    print('Use {}'.format(abs(end-start)))
+
+if __name__ == '__main__':
+    main()
---
+++ src/helperDP.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib_venn import venn2, venn3
+import seaborn as sns
+
+import sklearn
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.linear_model import LassoCV, RandomizedLasso, LassoLarsCV, lasso_stability_path
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import IncrementalPCA
+
+import statsmodels
+from statsmodels.sandbox.stats.multicomp import multipletests
+import statsmodels.api as sm
+from statsmodels.graphics.api import abline_plot
+import warnings
+
+import sys
+import os
+
+sns.set(style="white")
+
+# helper functions
+def load_coverage(path_coverage):
+    ''' Load covergae table. Return a pd.DF with binID as the key
+    '''
+    cg = pd.read_csv(path_coverage, sep='\t', header=0)
+    for col in ['chrom', 'start', 'end']:
+        if col in cg.columns.values:
+            del cg[col]
+    cg = cg.set_index('binID')
+    cg.sort_index(inplace=True)
+    assert cg.shape[1] == 32, "Number of columns (3-mer) for coverage table is not 32." # ncol = 32
+    return cg
+
+def load_covar(path_covar):
+    ''' Load pre-processed covar. table. Return a pd.DF with binID as the key
+    '''
+    cv = pd.read_csv(path_covar)
+    cv = cv.set_index('binID')
+    cv.sort_index(inplace=True)
+    return cv
+
+def load_count(path_count):
+    ''' Load mutatin count data. 5 cols TSV file ['binID', 'sid', 'context', 'to', 'ct']
+    '''
+    ct = pd.read_csv(path_count, sep='\t', header=None, names=['binID', 'sid', 'context', 'to', 'ct'])
+    return ct
+
+def load_all(path_cg, path_ct, path_cv):
+    cg = load_coverage(path_cg)
+    ct = load_count(path_ct)
+    cv = load_covar(path_cv)
+    assert np.array_equal(cv.index, cg.index), 'CV and CG binIDs do not match'
+    print('- CT: ', ct.shape)
+    print('- CG: ', cg.shape)
+    print('- CV: ', cv.shape)
+    return (cg, ct, cv)
+
+def get_response(ct, cg):
+    # Response - Test data
+    ct_byb = ct.pivot_table(values='ct', index='binID', aggfunc=sum) # count by binID
+    cg_sum = cg.sum(axis=1).copy()
+    cg_sum.name = 'cg'
+    ct_byb = pd.concat([ct_byb, cg_sum], axis=1)
+    ct_byb.fillna(0, inplace=True)
+    assert np.array_equal(ct_byb.index, cg.index), 'ct index does not match cg index'
+    # binom GLM responses (# success, # failure)
+    ybinom = np.array([ct_byb.ct, ct_byb.cg - ct_byb.ct]).T
+    print('- Binom Response shape: ', ybinom.shape)
+    return ybinom
+
+def get_filter(ct, cg, len_threshold=500, recur_threshold=2, return_recur=False):
+    # ct by binID and by sid
+    ct_byb_bys = ct.pivot_table(values='ct', index=['binID'], columns=['sid'], aggfunc=sum)
+    ct_byb_bys.fillna(0, inplace=True)
+    # Sum of cg per binID
+    cg_sum = cg.sum(axis=1).copy()
+    print('{} bins in input'.format(cg_sum.shape[0]))
+    # Recurrence table
+    recur = (ct_byb_bys > 0).sum(axis=1)
+    filter_tab = pd.concat([cg_sum, recur], axis=1)
+    filter_tab.columns = ['cg', 'recur']
+    filter_tab.fillna(0, inplace=True) # recur can be 0
+    assert np.array_equal(filter_tab.index, cg.index), "Filter index does not match CG index"
+    # 1. at least len_threshold bp
+    keep1 = np.where(filter_tab.cg >= len_threshold)[0] # index of bins pass
+    print('- {} bins >= {} bp'.format(keep1.shape[0], len_threshold))
+    # 2. at least mutated in recur_threshold samples
+    keep2 = np.where(filter_tab.recur >= recur_threshold)[0] # index of bins pass
+    print('- {} bins have mutations in at least {} samples'.format(keep2.shape[0], recur_threshold))
+    # intersection of keep1 and keep2
+    keep = np.intersect1d(keep1, keep2)
+    print('- {} ({:.2f}%) bins passed all filters'.format(keep.shape[0], keep.shape[0]/cg_sum.shape[0] * 100))
+    if return_recur:
+        return keep, filter_tab.recur
+    else:
+        return keep
+
+def apply_filter(keep, xy_list):
+    ''' Apply filter from get_filter() to X and Y list
+    Args:
+        keep - return value from get_filter. Row indexes that pass filters
+        xy_list - list of numpy ndarray. Could be 1D or 2D. Only rows will be selected.
+    Return:
+        xy_filtered - list of numpy ndarray.
+    '''
+    xy_filtered = []
+    for myarr in xy_list:
+        xy_filtered.append(myarr[keep])
+    return xy_filtered
+
+def plot_corr(pred, obs):
+    ''' Plot predicted value vs. observed value
+    '''
+    rval = sp.stats.pearsonr(pred, obs)[0]
+    fig, ax = plt.subplots()
+    ax.scatter(pred, obs)
+    abline_plot(ax=ax, slope=1, intercept=0)
+    ax.set_title('R = {}'.format(rval))
+    ax.set_ylabel('Observed values')
+    ax.set_xlabel('Predictd values')
+    ax.set_xlim([min(min(pred), min(obs)), max(max(pred), max(obs))])
+    ax.set_ylim([min(min(pred), min(obs)), max(max(pred), max(obs))])
+    return fig, ax
+
+def bar_plot(heights, labels):
+    ''' Plot a barplot with vertical labels
+    '''
+    fig, ax = plt.subplots()
+    n = len(heights)
+    ax.bar(np.arange(n), heights)
+    ax.set_xticks(np.arange(n) + 0.5)
+    ax.set_xticklabels(labels, rotation='vertical')
+    return fig, ax
+
+def pval_qqplot(pvals, labels=None):
+    ''' Plot a pval qqplot and label top points when provided.
+    Args:
+        pvals - List or np.array of pvals
+        labels - List or np.array of top point labels. Default None.
+    '''
+    obs_pval = -np.log10(np.sort(pvals))
+    exp_pval = -np.log10( np.arange(1, len(obs_pval)+1)/float(len(obs_pval+1)))
+    fig, ax = plt.subplots()
+    ax.scatter(exp_pval, obs_pval)
+    abline_plot(ax=ax, slope=1, intercept=0)
+    ax.set_ylabel('Observed p-values')
+    ax.set_xlabel('Expected p-values')
+    if labels:
+        ntop = len(labels)
+        for g, x, y in zip(labels, exp_pval[:ntop], obs_pval[:ntop]):
+            ax.annotate(g, xy=(x+0.1, y+0.1))
+    return fig, ax
+
+def split_by_cg(cg_train, cg_test, fold=4):
+    '''
+    '''
+    # Output format. Each row corresponds to one fold.
+    train_spliter = np.zeros((fold, cg_train.shape[0]), dtype=bool)
+    test_spliter  = np.zeros((fold, cg_test.shape[0]), dtype=bool)
+    q   = np.linspace(0,100,fold+1)
+    cut = np.percentile(cg_train, q)
+    cut[0] = cut[0] - 1
+    cut[fold] = cut[fold] + 1
+    for i in np.arange(fold):
+        train_spliter[i, :] = np.logical_and(cg_train >= cut[i], cg_train < cut[i+1])
+        test_spliter[i, :]  = np.logical_and(cg_test >= cut[i], cg_test < cut[i+1])
+    assert np.sum(train_spliter.sum(1)) == cg_train.shape[0]
+    assert np.sum(test_spliter.sum(1)) == cg_test.shape[0]
+    return train_spliter, test_spliter
