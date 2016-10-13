25667f25d057fac61e830d242e60b5b4f209db5e
Shimin Shuai <shuaishimin@gmail.com>
2016-10-13 18:18:21 -0400

    Make a new plotDP.py
    Move plot functions from helperDP to plotDP

--- src/helperDP.py
+++ src/helperDP.py
@@ -2,21 +2,11 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 
-import matplotlib.pyplot as plt
-import matplotlib
-from matplotlib_venn import venn2, venn3
-import seaborn as sns
-
-import sklearn
 from sklearn.preprocessing import StandardScaler, RobustScaler
-from sklearn.linear_model import LassoCV, RandomizedLasso, LassoLarsCV, lasso_stability_path
-from sklearn.model_selection import train_test_split
-from sklearn.decomposition import IncrementalPCA
+from sklearn.linear_model import LassoCV, RandomizedLasso
 
-import statsmodels
 from statsmodels.sandbox.stats.multicomp import multipletests
 import statsmodels.api as sm
-from statsmodels.graphics.api import abline_plot
 import warnings
 
 import sys
@@ -114,48 +104,6 @@ def apply_filter(keep, xy_list):
         xy_filtered.append(myarr[keep])
     return xy_filtered
 
-def plot_corr(pred, obs):
-    ''' Plot predicted value vs. observed value
-    '''
-    rval = sp.stats.pearsonr(pred, obs)[0]
-    fig, ax = plt.subplots()
-    ax.scatter(pred, obs)
-    abline_plot(ax=ax, slope=1, intercept=0)
-    ax.set_title('R = {}'.format(rval))
-    ax.set_ylabel('Observed values')
-    ax.set_xlabel('Predictd values')
-    ax.set_xlim([min(min(pred), min(obs)), max(max(pred), max(obs))])
-    ax.set_ylim([min(min(pred), min(obs)), max(max(pred), max(obs))])
-    return fig, ax
-
-def bar_plot(heights, labels):
-    ''' Plot a barplot with vertical labels
-    '''
-    fig, ax = plt.subplots()
-    n = len(heights)
-    ax.bar(np.arange(n), heights)
-    ax.set_xticks(np.arange(n) + 0.5)
-    ax.set_xticklabels(labels, rotation='vertical')
-    return fig, ax
-
-def pval_qqplot(pvals, labels=None):
-    ''' Plot a pval qqplot and label top points when provided.
-    Args:
-        pvals - List or np.array of pvals
-        labels - List or np.array of top point labels. Default None.
-    '''
-    obs_pval = -np.log10(np.sort(pvals))
-    exp_pval = -np.log10( np.arange(1, len(obs_pval)+1)/float(len(obs_pval+1)))
-    fig, ax = plt.subplots()
-    ax.scatter(exp_pval, obs_pval)
-    abline_plot(ax=ax, slope=1, intercept=0)
-    ax.set_ylabel('Observed p-values')
-    ax.set_xlabel('Expected p-values')
-    if labels:
-        ntop = len(labels)
-        for g, x, y in zip(labels, exp_pval[:ntop], obs_pval[:ntop]):
-            ax.annotate(g, xy=(x+0.1, y+0.1))
-    return fig, ax
 
 def split_by_cg(cg_train, cg_test, fold=4):
     '''
