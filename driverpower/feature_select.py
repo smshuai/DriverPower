a16d489a8ba8125fcf16eb4d655a6562907df39a
Shimin Shuai <shuaishimin@gmail.com>
2016-10-29 15:46:29 -0400

    fix indent with 4 spaces
    Former-commit-id: 5ee092ed8952dd4eb5f098bfc9bccd27911c7f6d

--- driverpower/feature_select.py
+++ driverpower/feature_select.py
@@ -21,32 +21,32 @@ logger = logging.getLogger('FEATURE SELECT')
 
 
 def run_lasso(X_train, ybinom_train, max_iter=3000, cv=5):
-	''' Implement LassoCV provided by sklearn
-	Note:
-		To generate alpha for rnalasso, use max_iter=3000 and cv=5
-		For feature selection purpose, use max_iter=3000 and cv=10
-	'''
-	logger.info('Implementing LassoCV with {} iter. and {}-fold CV'.format(max_iter, cv))
-	# generate logit response
-	ylogit_train = logit(ybinom_train[:,0]/ybinom_train.sum(1))
-	clf = LassoCV(max_iter=max_iter, cv=cv)
-	lassocv = clf.fit(X_train, ylogit_train)
-	logger.info('LassoCV alpha = {}'.format(lassocv.alpha_))
-	return lassocv
+    ''' Implement LassoCV provided by sklearn
+    Note:
+        To generate alpha for rnalasso, use max_iter=3000 and cv=5
+        For feature selection purpose, use max_iter=3000 and cv=10
+    '''
+    logger.info('Implementing LassoCV with {} iter. and {}-fold CV'.format(max_iter, cv))
+    # generate logit response
+    ylogit_train = logit(ybinom_train[:,0]/ybinom_train.sum(1))
+    clf = LassoCV(max_iter=max_iter, cv=cv)
+    lassocv = clf.fit(X_train, ylogit_train)
+    logger.info('LassoCV alpha = {}'.format(lassocv.alpha_))
+    return lassocv
 
 
 def run_rndlasso(X_train, ybinom_train, alpha,
-	n_resampling=200, sample_fraction=0.4):
-	''' Implement RandomizedLasso provided by sklearn
-	'''
-	logger.info('Implementing Randomized Lasso with alpha={}, n_resampling={} and sample_fraction={}'.format(alpha, n_resampling, sample_fraction))
-	# generate logit response
-	ylogit_train = logit(ybinom_train[:,0]/ybinom_train.sum(1))
-	clf = RandomizedLasso(alpha=alpha, n_resampling=n_resampling,
-		sample_fraction=sample_fraction,
-		selection_threshold=1e-3, normalize=False)
-	rndlasso = clf.fit(X_train, ylogit_train)
-	return rndlasso
+    n_resampling=200, sample_fraction=0.4):
+    ''' Implement RandomizedLasso provided by sklearn
+    '''
+    logger.info('Implementing Randomized Lasso with alpha={}, n_resampling={} and sample_fraction={}'.format(alpha, n_resampling, sample_fraction))
+    # generate logit response
+    ylogit_train = logit(ybinom_train[:,0]/ybinom_train.sum(1))
+    clf = RandomizedLasso(alpha=alpha, n_resampling=n_resampling,
+        sample_fraction=sample_fraction,
+        selection_threshold=1e-3, normalize=False)
+    rndlasso = clf.fit(X_train, ylogit_train)
+    return rndlasso
 
 def feature_score(fscores, fnames, cutoff):
     ''' Select features based on scores and cutoff.
@@ -65,22 +65,22 @@ def feature_score(fscores, fnames, cutoff):
 
 
 def fselect(X_train, X_test, ybinom_train, fnames, method='rndlasso', cutoff_rndlasso=0.5, cutoff_lasso=0.001):
-	''' Main wrapper function for feature selection
-	'''
-	support_method = ['rndlasso', 'lassocv']
-	assert method in support_method, 'Invalid feature selection method. Must be chosen from {}'.format(support_method)
-	if method == 'rndlasso':
-		# find alpha
-		lassocv  = run_lasso(X_train, ybinom_train, max_iter=3000, cv=5)
-		# run rndlasso
-		rndlasso = run_rndlasso(X_train, ybinom_train, alpha=lassocv.alpha_)
-		# feature importance
-		fscores = rndlasso.scores_
-		cutoff = cutoff_rndlasso
-	if method == 'lassocv':
-		lassocv = run_lasso(X_train, ybinom_train, max_iter=3000, cv=10)
-		fscores = np.abs(lassocv.coef_)
-		cutoff = cutoff_lasso
-	fset, fidx = feature_score(fscores, fnames, cutoff)
-	logger.info('At cutoff={}, {} selected features are: {}'.format(cutoff, len(fset), ", ".join(fset)))
-	return X_train[:, fidx], X_test[:, fidx], fscores
+    ''' Main wrapper function for feature selection
+    '''
+    support_method = ['rndlasso', 'lassocv']
+    assert method in support_method, 'Invalid feature selection method. Must be chosen from {}'.format(support_method)
+    if method == 'rndlasso':
+        # find alpha
+        lassocv  = run_lasso(X_train, ybinom_train, max_iter=3000, cv=5)
+        # run rndlasso
+        rndlasso = run_rndlasso(X_train, ybinom_train, alpha=lassocv.alpha_)
+        # feature importance
+        fscores = rndlasso.scores_
+        cutoff = cutoff_rndlasso
+    if method == 'lassocv':
+        lassocv = run_lasso(X_train, ybinom_train, max_iter=3000, cv=10)
+        fscores = np.abs(lassocv.coef_)
+        cutoff = cutoff_lasso
+    fset, fidx = feature_score(fscores, fnames, cutoff)
+    logger.info('At cutoff={}, {} selected features are: {}'.format(cutoff, len(fset), ", ".join(fset)))
+    return X_train[:, fidx], X_test[:, fidx], fscores
--- driverpower/model.py
+++ driverpower/model.py
@@ -24,39 +24,39 @@ logger = logging.getLogger('MODEL')
 
 
 def run_glm(X_train, ybinom_train, X_test):
-	''' Run binomial glm in statsmodels
-	'''
-	X_train = sm.add_constant(X_train, prepend=False)
-	X_test  = sm.add_constant(X_test, prepend=False)
-	glm = sm.GLM(ybinom_train, X_train, family=sm.families.Binomial())
-	glm_res = glm.fit()
-	mu_pred = glm_res.predict(X_test)
-	return mu_pred
+    ''' Run binomial glm in statsmodels
+    '''
+    X_train = sm.add_constant(X_train, prepend=False)
+    X_test  = sm.add_constant(X_test, prepend=False)
+    glm = sm.GLM(ybinom_train, X_train, family=sm.families.Binomial())
+    glm_res = glm.fit()
+    mu_pred = glm_res.predict(X_test)
+    return mu_pred
 
 
 def raw_test(mu_pred, ybinom_test, gnames, grecur):
-	''' Perform binomial test with functional adjustment
-	'''
-	res = pd.DataFrame({'binID': gnames, 'Length': ybinom_test.sum(1).astype(np.int),
-		'nMut':ybinom_test[:, 0].astype(np.int), 'nSample': grecur.astype(np.int), 'Mu': mu_pred})
-	# Raw pval. binom(nMut, length, mu_pred)
-	res['PvalRaw'] = [binom_test(x, n, p, 'greater') for x, n, p in zip(res.nMut, res.Length, res.Mu)]
-	res['QvalRaw'] = multipletests(res['PvalRaw'], method='fdr_bh')[1]
-	# Mean = (nSample + nMut) / 2
-	res['nMutSample'] = (res.nSample + res.nMut)/2
-	res['nMutSample'] = res.nMutSample.astype(np.int)
-	# PvalM. binom(nMutSample, length, mu_pred)
-	res['PvalM'] = [binom_test(x, n, p, 'greater') for x, n, p in zip(res.nMutSample, res.Length, res.Mu)]
-	res['QvalM'] = multipletests(res.PvalM, method='fdr_bh')[1]
-	# indexed by binID
-	res.set_index('binID', inplace=True)
-	return res
+    ''' Perform binomial test with functional adjustment
+    '''
+    res = pd.DataFrame({'binID': gnames, 'Length': ybinom_test.sum(1).astype(np.int),
+        'nMut':ybinom_test[:, 0].astype(np.int), 'nSample': grecur.astype(np.int), 'Mu': mu_pred})
+    # Raw pval. binom(nMut, length, mu_pred)
+    res['PvalRaw'] = [binom_test(x, n, p, 'greater') for x, n, p in zip(res.nMut, res.Length, res.Mu)]
+    res['QvalRaw'] = multipletests(res['PvalRaw'], method='fdr_bh')[1]
+    # Mean = (nSample + nMut) / 2
+    res['nMutSample'] = (res.nSample + res.nMut)/2
+    res['nMutSample'] = res.nMutSample.astype(np.int)
+    # PvalM. binom(nMutSample, length, mu_pred)
+    res['PvalM'] = [binom_test(x, n, p, 'greater') for x, n, p in zip(res.nMutSample, res.Length, res.Mu)]
+    res['QvalM'] = multipletests(res.PvalM, method='fdr_bh')[1]
+    # indexed by binID
+    res.set_index('binID', inplace=True)
+    return res
 
 
 def model(X_train, ybinom_train, X_test, ybinom_test, gnames, grecur, method='glm'):
-	support_method = ['glm']
-	assert method in support_method, 'Invalid model type. Must be chosen from {}'.format(support_method)
-	if method == 'glm':
-		mu_pred = run_glm(X_train, ybinom_train, X_test)
-	res = raw_test(mu_pred, ybinom_test, gnames, grecur)
-	return res
+    support_method = ['glm']
+    assert method in support_method, 'Invalid model type. Must be chosen from {}'.format(support_method)
+    if method == 'glm':
+        mu_pred = run_glm(X_train, ybinom_train, X_test)
+    res = raw_test(mu_pred, ybinom_test, gnames, grecur)
+    return res
