df092d5886d90caaaf14c8bc7ec7da17a6b9e0ff
Shimin Shuai <shuaishimin@gmail.com>
2016-11-22 16:53:20 -0500

    Add interactive plots and query system for other methods
    Former-commit-id: 94b71e36c6a958c14b5cadf82c7c39957884f55a

--- results/server.R
+++ results/server.R
@@ -1,5 +1,4 @@
 library(shiny)
-source('./qqunif.plot.R')
 library(ggplot2)
 # Define server logic required to draw a histogram
 function(input, output, session) {
@@ -12,10 +11,11 @@ function(input, output, session) {
   meta = read.table(
     './feature_meta.v2.tsv',
     header = TRUE,
-    row.names = ,
     sep = '\t'
   )
   meta = meta[, c('feature_name' , 'description')]
+  # read project overview
+  project = read.table('./project_overview.tsv', header=TRUE, row.names = 'Project_Code')
   
   # show data table
   ## feature selection
@@ -58,7 +58,12 @@ function(input, output, session) {
     dat$fscore = signif(dat$fscore, 4)
     dat$Pval = signif(dat$Pval, 4)
     dat$Qval = signif(dat$Qval, 4)
-    dat[, c('binID', 'Length', 'nMut', 'nSample', 'MuAdj', 'fscore', 'Pval', 'Qval')]
+    dat = dat[, c('binID', 'Length', 'nMut', 'nSample', 'MuAdj', 'fscore', 'Pval', 'Qval')]
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10( 1:length(dat$o)/length(dat$o))
+    dat
   })
   # feature selection table
   output$table <- DT::renderDataTable({
@@ -71,14 +76,381 @@ function(input, output, session) {
 
   output$qqplot <- renderPlot({
     dat = resDat()
-    qqunif.plot(dat$Pval)
+    genes = dat[dat$Qval<=0.1, ]
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
   })
   
-  output$info <- renderPrint({
+  output$summary <- renderText({
     dat = resDat()
-    nearPoints(dat, input$plot_click, threshold = 10, maxpoints = 1,
-               addDist = TRUE)
+    genes = dat[dat$Qval<=0.1, ]
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$tumor2, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$tumor2, 'WG_Mu'])
+      )
+    
   })
+  
+  output$pt_dp <- renderPrint({
+    dat = resDat()
+    
+    nearPoints(dat, input$plot_click, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$br_dp <- renderPrint({
+    dat = resDat()
+    brushedPoints(dat, input$plot_brush)
+  })
+  
+  # oncodriveFML (odf)
+  odfDat <- reactive({
+    dat = read.table(paste0(input$odfType,'/oncodriveFML/',input$oncodrive,'/',input$odfTumor,'.CDS.oncodriveFML.observed.txt'),
+                     header=TRUE, sep='\t')
+    dat[, 'element_ID'] = tstrsplit(dat$element_ID, '::', fixed = T)[[3]]
+    dat = dat[, c('element_ID', 'p.value', 'q.value')]
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$odfTab <- DT::renderDataTable({
+    odfDat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))
+  output$odfPlot <- renderPlot({
+    dat = odfDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$odfSummary <- renderText({
+    dat = odfDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$odfTumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$odfTumor, 'WG_Mu'])
+    )
+  })
+  
+  output$odfPt <- renderPrint({
+    dat = odfDat()
+    
+    nearPoints(dat, input$odfClick, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$odfBr <- renderPrint({
+    dat = odfDat()
+    brushedPoints(dat, input$odfBrush)
+  })
+  
+  # ActiveDriver2 (ad2)
+  ad2Dat <- reactive({
+    dat = read.table(paste0(input$ad2Type,'/ActiveDriver2/',input$ad2Tumor,'.gc19_pc.cds.ActiveDriver2-burden.observed.txt'),
+                     header=TRUE, sep='\t')
+    dat[, 'id'] = tstrsplit(dat$id, '::', fixed = T)[[3]]
+    dat[,'Qval'] = p.adjust(dat$raw_p, method = 'BH')
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    rownames(dat) = 1:nrow(dat)
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$ad2Tab <- DT::renderDataTable({
+    ad2Dat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))  
+  output$ad2Plot <- renderPlot({
+    dat = ad2Dat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$ad2Summary <- renderText({
+    dat = ad2Dat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$ad2Tumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$ad2Tumor, 'WG_Mu'])
+    )
+  })
+  
+  output$ad2Pt <- renderPrint({
+    dat = ad2Dat()
+    
+    nearPoints(dat, input$ad2Click, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$ad2Br <- renderPrint({
+    dat = ad2Dat()
+    brushedPoints(dat, input$ad2Brush)
+  })
+  
+  # compositeDriver (cd)
+  cdDat <- reactive({
+    dat = read.table(paste0(input$cdType,'/compositeDriver/',input$cdTumor,'.CDS.compositeDriver.observed.txt'),
+                     header=TRUE, sep='\t')
+    dat[, 'element_ID'] = tstrsplit(dat$element_ID, '::', fixed = T)[[3]]
+    dat[,'Qval'] = p.adjust(dat$pvalue, method = 'BH')
+    dat = dat[, c('element_ID', 'pvalue', 'Qval')]
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    rownames(dat) = 1:nrow(dat)
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$cdTab <- DT::renderDataTable({
+    cdDat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))  
+  output$cdPlot <- renderPlot({
+    dat = cdDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$cdSummary <- renderText({
+    dat = cdDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$cdTumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$cdTumor, 'WG_Mu'])
+    )
+  })
+  
+  output$cdPt <- renderPrint({
+    dat = cdDat()
+    
+    nearPoints(dat, input$ad2Click, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$cdBr <- renderPrint({
+    dat = cdDat()
+    brushedPoints(dat, input$cdBrush)
+  })
+  
+  # ExInAtor (eia)
+  eiaDat <- reactive({
+    dat = read.table(paste0(input$eiaType,'/ExInAtor/',input$eiaTumor,'.CDS.ExInAtor.observed.txt'),
+                     header=TRUE, sep='\t', check.names = FALSE)
+    dat[, 'element_ID'] = tstrsplit(dat$element_ID, '::', fixed = T)[[3]]
+    dat[,'Qval'] = p.adjust(dat[,'p-value'], method = 'BH')
+    dat = dat[, c('element_ID', 'p-value', 'Qval')]
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    rownames(dat) = 1:nrow(dat)
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$eiaTab <- DT::renderDataTable({
+    eiaDat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))  
+  output$eiaPlot <- renderPlot({
+    dat = eiaDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$eiaSummary <- renderText({
+    dat = eiaDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$eiaTumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$eiaTumor, 'WG_Mu'])
+    )
+  })
+  
+  output$eiaPt <- renderPrint({
+    dat = eiaDat()
+    
+    nearPoints(dat, input$eiaClick, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$eiaBr <- renderPrint({
+    dat = eiaDat()
+    brushedPoints(dat, input$eiaBrush)
+  })
+  
+  # ncdDetect (ncdd)
+  ncddDat <- reactive({
+    dat = read.table(paste0(input$ncddType,'/ncdDetect/',input$ncddTumor,'.CDS.ncdDetect.observed.txt'),
+                     header=TRUE, sep='\t', check.names = FALSE)
+    dat[, 'element_ID'] = tstrsplit(dat$element_ID, '::', fixed = T)[[3]]
+    dat[,'Qval'] = p.adjust(dat[,'p_value'], method = 'BH')
+    dat = dat[, c('element_ID', 'p_value', 'Qval')]
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    rownames(dat) = 1:nrow(dat)
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$ncddTab <- DT::renderDataTable({
+    ncddDat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))  
+  output$ncddPlot <- renderPlot({
+    dat = ncddDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$ncddSummary <- renderText({
+    dat = ncddDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$ncddTumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$ncddTumor, 'WG_Mu'])
+    )
+  })
+  
+  output$ncddPt <- renderPrint({
+    dat = ncddDat()
+    
+    nearPoints(dat, input$ncddClick, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$ncddBr <- renderPrint({
+    dat = ncddDat()
+    brushedPoints(dat, input$ncddBrush)
+  })
+  
+  # ncDriver (ncd)
+  ncdDat <- reactive({
+    dat = read.table(paste0(input$ncdType,'/ncDriver/',input$ncdTumor,'.gc19_pc.cds.ncDriverConservation.observed.snv.p.comb'),
+                     header=TRUE, sep='\t', check.names = FALSE)
+    dat[, 'id.element'] = tstrsplit(dat$id.element, '::', fixed = T)[[3]]
+    dat[,'Qval'] = p.adjust(dat[,'conservation.p.comb'], method = 'BH')
+    dat = dat[, c('id.element', 'conservation.p.comb', 'Qval')]
+    colnames(dat) = c('binID', 'Pval', 'Qval')
+    # order by pvalue
+    dat = dat[order(dat$Pval, decreasing = F),]
+    rownames(dat) = 1:nrow(dat)
+    dat[, 'o'] = -log10(dat$Pval)
+    dat[, 'e'] = -log10(1:length(dat$o)/length(dat$o))
+    dat
+  })
+  output$ncdTab <- DT::renderDataTable({
+    ncdDat()
+  }, options = list(rownames = TRUE,
+                    order = list(3, 'asc')))  
+  output$ncdPlot <- renderPlot({
+    dat = ncdDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    p = ggplot(data = dat, aes(x=e, y=o)) + geom_point() + geom_abline(intercept=0,slope=1, col="red")
+    p = p + xlab(expression(Expected~~-log[10](italic(p)))) +
+      ylab(expression(Observed~~-log[10](italic(p)))) +
+      theme(axis.text=element_text(size=12),
+            axis.title=element_text(size=14,face="bold"))
+    p = p + geom_text(data = genes, aes(x=e, y=o, label = binID), check_overlap = TRUE, hjust = 0, nudge_x = 0.1)
+    p
+  })
+  output$ncdSummary <- renderText({
+    dat = ncdDat()
+    genes = dat[dat$Qval<=0.1, ]
+    genes = na.omit(genes)
+    paste0(
+      "Number of samples: ",
+      as.character(project[input$ncdTumor, 'Num_Donors']),
+      "\nNumber of significant (q<0.1) bins: ",
+      as.character(nrow(genes)),
+      "\nOverall mutation rate (/Mbp): ",
+      as.character(project[input$ncdTumor, 'WG_Mu'])
+    )
+  })
+  
+  output$ncdPt <- renderPrint({
+    dat = ncdDat()
+    
+    nearPoints(dat, input$ncdClick, threshold = 10, maxpoints = 1,
+               addDist = FALSE)
+  })
+  
+  output$ncdBr <- renderPrint({
+    dat = ncdDat()
+    brushedPoints(dat, input$ncdBrush)
+  })
+  
 }
 
 
--- results/ui.R
+++ results/ui.R
@@ -4,22 +4,29 @@ library(data.table)
 
 navbarPage("Query DriverPower",
            tabPanel("Feature Selection",
-                    selectInput(
-                      "tumor",
-                      "Choose a tumor:",
-                      choices = c(
-                        "ColoRect-AdenoCA",
-                        "Skin-Melanoma",
-                        "Liver-HCC",
-                        "Eso-AdenoCa",
-                        "Lung-SCC",
-                        "Stomach-AdenoCA",
-                        "Panc-AdenoCA",
-                        'Lung-AdenoCA'
-                      )
+                    fluidRow(
+                      column(
+                        3,
+                        selectInput(
+                          "tumor",
+                          "Choose a tumor:",
+                          choices = c(
+                            "ColoRect-AdenoCA",
+                            "Skin-Melanoma",
+                            "Liver-HCC",
+                            "Eso-AdenoCa",
+                            "Lung-SCC",
+                            "Stomach-AdenoCA",
+                            "Panc-AdenoCA",
+                            'Lung-AdenoCA'
+                          )
+                        )),
+                      column(
+                        3,
+                        radioButtons('response', "Choose a response:",
+                                     choices = c("gmean", "count"), selected = 'gmean')
+                        )
                     ),
-                    radioButtons('response', "Choose a response:",
-                                choices = c("gmean", "count"), selected = 'gmean'),
                     DT::dataTableOutput("table")
            ),
            tabPanel("DriverPower",
@@ -64,9 +71,312 @@ navbarPage("Query DriverPower",
                       ),
                       tabPanel(
                         "Plot",
-                        plotOutput('qqplot'),
-                        verbatimTextOutput("info")
+                        fluidRow(
+                          column(
+                            6,
+                            plotOutput('qqplot', width = '600px', height = '600px', click = "plot_click", brush = "plot_brush")
+                            ),
+                          column(
+                            6,
+                            verbatimTextOutput("summary"),
+                            verbatimTextOutput("pt_dp"),
+                            verbatimTextOutput("br_dp")
+                          )
+                        )
                       )
                     )
+           ),
+           tabPanel(
+             'OncodriveFML',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "odfType",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "odfTumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      )),
+               column(
+                 3,
+                 radioButtons('oncodrive', "Choose a method:",
+                              choices = c("cadd", "vest3"), selected = 'cadd')
+               )
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('odfTab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('odfPlot', width = '600px', height = '600px', click = "odfClick", brush = "odfBrush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("odfSummary"),
+                     verbatimTextOutput("odfPt"),
+                     verbatimTextOutput("odfBr")
+                   )
+                 )
+               )
+             )
+           ),
+           tabPanel(
+             'ActiveDriver2',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "ad2Type",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "ad2Tumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      ))
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('ad2Tab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('ad2Plot', width = '600px', height = '600px', click = "ad2Click", brush = "ad2Brush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("ad2Summary"),
+                     verbatimTextOutput("ad2Pt"),
+                     verbatimTextOutput("ad2Br")
+                   )
+                 )
+               )
+             )
+           ),
+           tabPanel(
+             'compositeDriver',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "cdType",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "cdTumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      ))
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('cdTab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('cdPlot', width = '600px', height = '600px', click = "cdClick", brush = "cdBrush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("cdSummary"),
+                     verbatimTextOutput("cdPt"),
+                     verbatimTextOutput("cdBr")
+                   )
+                 )
+               )
+             )
+           ),
+           tabPanel(
+             'ExInAtor',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "eiaType",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "eiaTumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      ))
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('eiaTab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('eiaPlot', width = '600px', height = '600px', click = "eiaClick", brush = "eiaBrush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("eiaSummary"),
+                     verbatimTextOutput("eiaPt"),
+                     verbatimTextOutput("eiaBr")
+                   )
+                 )
+               )
+             )
+           ),
+           tabPanel(
+             'ncdDetect',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "ncddType",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "ncddTumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      ))
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('ncddTab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('ncddPlot', width = '600px', height = '600px', click = "ncddClick", brush = "ncddBrush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("ncddSummary"),
+                     verbatimTextOutput("ncddPt"),
+                     verbatimTextOutput("ncddBr")
+                   )
+                 )
+               )
+             )
+           ),
+           tabPanel(
+             'ncDriver',
+             fluidRow(
+               column(3,
+                      selectInput(
+                        "ncdType",
+                        "Choose a category:",
+                        choices = c("exon"),
+                        selected = 'exon'
+                      )),
+               column(3,
+                      selectInput(
+                        "ncdTumor",
+                        "Choose a tumor:",
+                        choices = c(
+                          "ColoRect-AdenoCA",
+                          "Skin-Melanoma",
+                          "Liver-HCC",
+                          "Eso-AdenoCa",
+                          "Lung-SCC",
+                          "Stomach-AdenoCA",
+                          "Panc-AdenoCA",
+                          'Lung-AdenoCA'
+                        ), selected = "ColoRect-AdenoCA"
+                      ))
+             ),
+             tabsetPanel(
+               tabPanel(
+                 'Table',
+                 DT::dataTableOutput('ncdTab')
+               ),
+               tabPanel(
+                 'Plot',
+                 fluidRow(
+                   column(
+                     6,
+                     plotOutput('ncdPlot', width = '600px', height = '600px', click = "ncdClick", brush = "ncdBrush")
+                   ),
+                   column(
+                     6,
+                     verbatimTextOutput("ncdSummary"),
+                     verbatimTextOutput("ncdPt"),
+                     verbatimTextOutput("ncdBr")
+                   )
+                 )
+               )
+             )
            )
 )
