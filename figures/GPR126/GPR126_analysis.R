889b8a37a6bff1a30d8618622975393c1a6db1dc
Shimin Shuai <shuaishimin@gmail.com>
2017-02-17 17:00:19 -0500

    More GPR126
    Former-commit-id: b76a0045485e36745154a975a2e70a9829586115

--- figures/GPR126/GPR126_analysis.R
+++ figures/GPR126/GPR126_analysis.R
@@ -2,16 +2,56 @@
 # Bladder-TCC
 # No coding mutations
 setwd('~/Desktop/DriverPower/figures/')
+source('./ggplot_theme.R')
 rnameta = read.table('./ALB/rnaseq_metadata.lite.tsv', header=T, sep='\t', stringsAsFactors = F)
 rnameta = rnameta[rnameta$project_code %in% c('BLCA-US'),]
 table(rnameta$is_tumour)
 library(data.table)
+library(ggplot2)
 rnaseq = fread('~/Downloads/tophat_star_fpkm_uq.v2_aliquot_gl.tsv', select = c('feature', rnameta$aliquot_id))
 rnaseq = as.data.frame(rnaseq)
 g = 'ENSG00000009844.11' # VTA1
 g = 'ENSG00000112414.10' # GPR126
 g = 'ENSG00000236366.1' # RP11-440G9.1
 g = 'ENSG00000266843.1'
+# VEGF pathway genes:
+# VEGFR1: gc19_pc.cds::gencode::FLT1::ENSG00000102755.6
+# VEGFR2: gc19_pc.cds::gencode::KDR::ENSG00000128052.8
+# VEGFR3: gc19_pc.cds::gencode::FLT4::ENSG00000037280.11
+# VEGFA: gc19_pc.cds::gencode::VEGFA::ENSG00000112715.16
+# VEGFB: gc19_pc.cds::gencode::VEGFB::ENSG00000173511.5
+# VEGFC: gc19_pc.cds::gencode::VEGFC::ENSG00000150630.2
+# VEGFD: gc19_pc.cds::gencode::FIGF::ENSG00000165197.4
+# PGF: gc19_pc.cds::gencode::PGF::ENSG00000119630.9
+
+plot_gene <- function(g, rnaseq, rnameta, donors.in, name){
+  g.exp = as.data.frame(t(rnaseq[rnaseq$feature==g,-1]))
+  g.exp['aliquot_id'] = row.names(g.exp)
+  g.exp = merge(g.exp, rnameta[,c(2,3,5)])
+  colnames(g.exp)[2:3] = c('FPKM.UQ', 'donor_ID')
+  g.exp['is_mut'] = ifelse(g.exp$donor_ID %in% donors.in, 'MUT', 'WT')
+  g.exp[g.exp$is_tumour=='no', 'is_mut'] = 'Normal'
+  g.exp$is_mut = factor(g.exp$is_mut, levels = c('Normal', 'WT', 'MUT'))
+  xlabs <- paste0(levels(g.exp$is_mut),"\n(N=",table(g.exp$is_mut),")")
+  ylabel = paste0("FPKM-UQ expression of ", name)
+  ggplot(g.exp, aes(x=is_mut, y=FPKM.UQ, fill=is_tumour)) + geom_boxplot() + ylab(ylabel) +
+    scale_fill_Publication1() + guides(fill=FALSE) +
+    theme_Publication() + scale_x_discrete(labels=xlabs) + theme(axis.title.x = element_blank())
+}
+plot_gene('ENSG00000112414.10', rnaseq, rnameta, donors.in, 'GPR126')
+# VEGFR
+plot_gene('ENSG00000102755.6', rnaseq, rnameta, donors.in, 'FLT1')
+plot_gene('ENSG00000128052.8', rnaseq, rnameta, donors.in, 'KDR')
+plot_gene('ENSG00000037280.11', rnaseq, rnameta, donors.in, 'FLT4')
+# VEGF
+plot_gene('ENSG00000112715.16', rnaseq, rnameta, donors.in, 'VEGFA')
+plot_gene('ENSG00000173511.5', rnaseq, rnameta, donors.in, 'VEGFB')
+plot_gene('ENSG00000150630.2', rnaseq, rnameta, donors.in, 'VEGFC')
+plot_gene('ENSG00000165197.4', rnaseq, rnameta, donors.in, 'VEGFD')
+plot_gene('ENSG00000119630.9', rnaseq, rnameta, donors.in, 'PGF')
+
+
+
 gpr126.expression = rnaseq[rnaseq$feature==g,]
 gpr126.expression = t(gpr126.expression[,-1])
 gpr126.expression = as.data.frame(gpr126.expression)
