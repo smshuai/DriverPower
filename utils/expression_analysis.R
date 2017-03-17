46bba7a16444719e0c2d34ec9ea5dbd6e2811004
Shimin Shuai <shuaishimin@gmail.com>
2017-03-17 17:30:25 -0400

    Update expression analysis
    Former-commit-id: bbcf587f037a4848723200ea0ebedb1dd290cd9c

--- utils/expression_analysis.R
+++ utils/expression_analysis.R
@@ -68,19 +68,119 @@ df_one_gene = function(donors.in, rna, rnameta, cnv, gene.name='WDR74', gene.id=
 }
 
 # example 1: WDR74
-donors.in = scan('~/Downloads/donors.in.txt', what = character())
+donors.in = scan('~/Downloads/donors.in.WDR74', what = character())
 df_one_gene(donors.in, rna, rnameta, cnv, 'WDR74', 'ENSG00000133316.11')
 df_one_gene(donors.in, rna, rnameta, cnv, 'ZFP36L2', 'ENSG00000152518.5')
 df_one_gene(donors.in, rna, rnameta, cnv, 'RN7SK', 'ENSG00000202198.1')
 df_one_gene(donors.in, rna, rnameta, cnv, 'FOXP2', 'ENSG00000128573.18')
 df_one_gene(donors.in, rna, rnameta, cnv, 'GATA2', 'ENSG00000179348.7')
-
 df_one_gene(donors.in, rna, rnameta, cnv, 'RNU1-2', 'ENSG00000207005.1')
 df_one_gene(donors.in, rna, rnameta, cnv, 'CROCC', 'ENSG00000058453.12')
-
 df_one_gene(donors.in, rna, rnameta, cnv, 'RNU6-9', 'ENSG00000207507.1')
 df_one_gene(donors.in, rna, rnameta, cnv, 'MED16', 'ENSG00000175221.10')
-
 df_one_gene(donors.in, rna, rnameta, cnv, 'CDK6', 'ENSG00000105810.5')
-
+df_one_gene(donors.in, rna, rnameta, cnv, 'RNU5A-1', 'ENSG00000199568.1')
 df_one_gene(donors.in, rna, rnameta, cnv, 'PARP16', 'ENSG00000138617.10')
+
+# res
+res = read.table('~/Downloads/Driver Regions - No-PCAWG set merged results.tsv',
+                 stringsAsFactors = F, header=T, sep='\t')
+targets = lapply(strsplit(res$PCAWG_elements, ','),
+       function(x) {
+         if (x[1] != '.'){
+         parts = tstrsplit(x, '::')
+         unique(paste(parts[[2]], parts[[3]], parts[[4]], sep = '::'))
+         }else{
+           x
+         }
+       }
+)
+targets.append = sapply(strsplit(res$PCAWG_elements, ','),
+                        function(x) {
+                          if (x[1] != '.'){
+                            parts = tstrsplit(x, '::')
+                            uniq_id = unique(parts[[3]])
+                            uniq_id = uniq_id[!grepl('NA', uniq_id)]
+                            paste(uniq_id, collapse =';')
+                          }else{
+                            x
+                          }
+                        })
+targets.append[104] = 'GPR126'
+res$Target = targets.append
+# no target regions 
+no_target = subset(res, Target == '.')
+no_regions = sapply(strsplit(no_target$Element_ID, ','), function(x) unique(tstrsplit(x, '::')[[1]]))
+no_regions = do.call(c, no_regions)
+table(no_regions)
+
+# uniq targets
+uniq_targets = unique(do.call(c, targets))
+# remove microRNA prom
+uniq_targets = uniq_targets[!grepl('^chr', uniq_targets)]
+# Add GPR126
+uniq_targets = c(uniq_targets, 'gencode::GPR126::ENSG00000112414.10')
+# 101 uniq targets in total
+
+# Compare normal expression vs tumour
+head(rnameta)
+normal_vs_tumour <- function(g.name, g.id, rnameta, rna, projects){
+  pvals = numeric(1+length(projects))
+  names(pvals) = c('PanCan_No_Melanoma_Lymph', projects)
+  g.rna = rna[rna$feature == g.id, -1]
+  if(nrow(g.rna) != 1) stop('RNA is not right with ', nrow(g.rna), ' row(s)')
+  g.rna = data.frame(t(g.rna), colnames(g.rna))
+  colnames(g.rna) = c('FPKM.UQ', 'aliquot_id')
+  dat = merge(g.rna, rnameta)
+  pval = LRT_test(dat)
+  pvals[1] = pval
+  p = plot_dat(dat, pval)
+  # pancan
+  p = p + ggtitle(paste0(g.name, " - ", 'PanCan_No_Melanoma_Lymph'))
+  print(p)
+  for (tumour in projects){
+    dat.p = subset(dat, project == tumour)
+    pval = LRT_test(dat.p)
+    pvals[tumour] = pval
+    p = plot_dat(dat.p, pval)
+    p = p + ggtitle(paste0(g.name, " - ", tumour))
+    print(p)
+  }
+  return(pvals)
+}
+
+rnameta$purity[rnameta$is_tumour=='no']=0
+LRT_test <- function(dat, form='FPKM.UQ ~ is_tumour + purity', plot_var='is_tumour'){
+  # LRT test
+  mod = glm(form, data = dat, family = quasipoisson())
+  pval = anova(mod, test = 'LRT')$`Pr(>Chi)`[2]
+  return(pval)
+}
+plot_dat <- function(dat, pval, plot_var='is_tumour'){
+  dat['logFPKM'] = log2(dat$FPKM.UQ+1)
+  dat[plot_var] = as.factor(dat[,plot_var])
+  xlabs <- paste0(levels(dat[,plot_var]),"\n(N=",table(dat[plot_var]),")")
+  p = ggplot(dat, aes_string(x=plot_var, y='logFPKM')) + geom_boxplot(outlier.shape = NA) +
+    geom_jitter(aes_string(color=plot_var)) +
+    ylab("FPKM-UQ expression (log2 scale)") + theme_Publication() + scale_color_npg() +
+    scale_x_discrete(labels=xlabs) + guides(color=FALSE) + theme(axis.title.x = element_blank()) +
+    annotate('text', x=-Inf, y=Inf, hjust=-.5, vjust=3,
+             label=paste0('LRT p-value: ', as.character(round(pval, 4))))
+  return(p)
+}
+
+ct = as.data.frame(table(rnameta$project, rnameta$is_tumour))
+projects = as.character(ct$Var1[ct$Var2=='no' & ct$Freq>=2])
+uniq_targets = uniq_targets[-25] # remove .
+write.table(uniq_targets,'~/Downloads/uniq_targets.txt', sep='\t', quote = F, row.names = F)
+uniq_targets = scan('~/Downloads/uniq_targets.txt', what = character())
+pdf('~/Downloads/exp_normal_vs_tumour.pdf')
+all_pvals = list()
+for (gene in uniq_targets){
+  gname = strsplit(gene, '::')[[1]][2]
+  gid = paste0(strsplit(gene, '::')[[1]][1], '::', gname, '::tc_', strsplit(gene, '::')[[1]][3])
+  print(gid)
+  all_pvals[gid] = normal_vs_tumour(gname, gid, rnameta, rna, projects)
+}
+dev.off()
+normal_vs_tumour('MIR663A', 'ENSG00000227195.4', rnameta, rna, projects)
